4d8b8581db0bdcf9580816ad06e835ce5833f377
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-05-15 16:53:24 +0300

    start app on the .env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { config } from 'src/config/config';
 
 async function bootstrap() {
+  const port = <number>config.get('app.port');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, () => {
+    Logger.log(`Listening for requests on port: ${port}`, 'NestApplication');
+  });
 }
 bootstrap();
