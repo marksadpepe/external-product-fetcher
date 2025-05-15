32c5bafd417f7340aab620c48ae12eb1d784a236
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-05-15 18:50:59 +0300

    enabled swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,23 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { config } from 'src/config/config';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const port = <number>config.get('app.port');
 
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port, () => {
     Logger.log(`Listening for requests on port: ${port}`, 'NestApplication');
   });
